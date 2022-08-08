e7c1bfb63655dadae27fe85a601bb21cfb86c02c
Ivan <ivan.frontcoder@gmail.com>
2022-08-08 13:11:34 +0500

    .

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,16 @@ window.onload = function () {
   // Vide | Видео на фоне
   const welcomeVideo = $('#welcomeVideo').vide({
     mp4: 'video/main_bg_x264.mp4',
-    poster: 'video/main_bg_x264.jpg'
+    // poster: 'video/main_bg_x264.jpg'
   }, {
-    posterType: 'jpg',
+    // posterType: 'jpg',
   });
 
   const videoBox = $('#VideoBox').vide({
     mp4: 'video/box_bg_x264.mp4',
-    poster: 'video/box_bg_x264.jpg'
+    // poster: 'video/box_bg_x264.jpg'
   }, {
-    posterType: 'jpg',
+    // posterType: 'jpg',
   });
 
   // JQueryInputmask | Маска для поля ввода телефона
